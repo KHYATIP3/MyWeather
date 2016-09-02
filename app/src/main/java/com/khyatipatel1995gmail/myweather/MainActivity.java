4ca0753c94223ace769b12713bb5d339476f26a0
KHYATIP3 <khyatipatel1995@gmail.com>
2016-09-02 18:23:09 +0530

    kfioojtrj

--- app/src/main/java/com/khyatipatel1995gmail/myweather/MainActivity.java
+++ app/src/main/java/com/khyatipatel1995gmail/myweather/MainActivity.java
@@ -1,7 +1,12 @@
 package com.khyatipatel1995gmail.myweather;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), (CharSequence) Environment.getExternalStorageDirectory().getAbsolutePath(),
+                Toast.LENGTH_LONG).show();
+         Log.e("","Hello world");
     }
+
+
 }
