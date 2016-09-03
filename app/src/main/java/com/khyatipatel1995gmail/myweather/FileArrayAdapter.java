6fb6e8efbb452222110710ef5fbcd4cbb7ce4f0c
KHYATIP3 <khyatipatel1995@gmail.com>
2016-09-03 11:33:02 +0530

    kfioojtrj

---
+++ app/src/main/java/com/khyatipatel1995gmail/myweather/FileArrayAdapter.java
@@ -0,0 +1,8 @@
+package com.khyatipatel1995gmail.myweather;
+
+/**
+ * Created by KHYATI PATEL on 9/2/2016.
+ */
+public class FileArrayAdapter {
+
+}
--- app/src/main/java/com/khyatipatel1995gmail/myweather/MainActivity.java
+++ app/src/main/java/com/khyatipatel1995gmail/myweather/MainActivity.java
@@ -1,22 +1,41 @@
 package com.khyatipatel1995gmail.myweather;
 
+import android.content.ClipData;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public ListView lv;
+    private FileArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), (CharSequence) Environment.getExternalStorageDirectory().getAbsolutePath(),
-                Toast.LENGTH_LONG).show();
-         Log.e("","Hello world");
+        lv = (ListView) findViewById(R.id.storage_listview);
+        String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File currentdir = new File(baseDir);
+        File[] allfiles = currentdir.listFiles();
+        List<String> dirs = new ArrayList<String>();
+        List<String> files = new ArrayList<String>();
+         for (File file: allfiles)
+         {
+             if (file.isDirectory()) {
+                 dirs.add(file.getName());
+             } else {
+                 files.add(file.getName());
+             }
+         }
+        Log.e("", "Hello world");
     }
 
 
