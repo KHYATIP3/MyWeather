f583240e34402db0690ce0a40d43014780b571fa
KHYATIP3 <khyatipatel1995@gmail.com>
2016-09-05 21:21:22 +0530

    kfioojtrj

--- app/src/main/java/com/khyatipatel1995gmail/myweather/MainActivity.java
+++ app/src/main/java/com/khyatipatel1995gmail/myweather/MainActivity.java
@@ -1,42 +1,73 @@
 package com.khyatipatel1995gmail.myweather;
 
-import android.content.ClipData;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    public ListView lv;
-    private FileArrayAdapter adapter;
+
+    private static final String TAG = "MainActivity";
+
+    public ListView explorerListView;
+    Button backButton;
+    List<String> files;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv = (ListView) findViewById(R.id.storage_listview);
+        explorerListView = (ListView) findViewById(R.id.storage_listview);
         String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
-        File currentdir = new File(baseDir);
-        File[] allfiles = currentdir.listFiles();
-        List<String> dirs = new ArrayList<String>();
-        List<String> files = new ArrayList<String>();
-         for (File file: allfiles)
-         {
-             if (file.isDirectory()) {
-                 dirs.add(file.getName());
-             } else {
-                 files.add(file.getName());
-             }
-         }
-        Log.e("", "Hello world");
-    }
+        File currentDir = new File(baseDir);
+        File[] allFiles = currentDir.listFiles();
+        files = new ArrayList<String>();
+        for (File file : allFiles) {
+            files.add(file.getPath());
+        }
+        Collections.sort(files);
+        final FileArrayAdapter filesAdapter = new FileArrayAdapter(getApplicationContext(), R.layout.listview_item_row, files);
+        View header = (View) getLayoutInflater().inflate(R.layout.listview_header, null);
+        backButton=(Button)header.findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+        explorerListView.addHeaderView(header);
+        explorerListView.setAdapter(filesAdapter);
+        explorerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                File clickedFile = new File(files.get(position));
+                if (clickedFile.isDirectory()) {
+                    File[] tempAllFiles = clickedFile.listFiles();
+                    files.clear();
+                    for (File file : tempAllFiles) {
+                        files.add(file.getPath());
+                    }
+                    FileArrayAdapter filesAdapter = new FileArrayAdapter(getApplicationContext(), R.layout.listview_item_row, files);
+                    explorerListView.setAdapter(filesAdapter);
+
+                } else {
+
+                }
+                Log.d(TAG, "on create:" + clickedFile);
+                Log.d(TAG, "onItemClick:" + files);
+            }
+        });
+
+
+    }
 
 }
